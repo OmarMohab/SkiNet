2713fabb3b75f2349522390a627c404b5e62386d
Omar Bakr <omar.bakr@sita.aero>
2025-07-27 20:39:01 +0300

    Adding Docker-SqlServer

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -7,4 +7,11 @@ namespace Infrastructure.Data;
 public class StoreContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly( typeof(ProductConfiguration).Assembly );
+    }
 }
