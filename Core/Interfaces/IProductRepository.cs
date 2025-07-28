904eace481ccdd129795fc46ce45e51117839044
Omar Bakr <omar.bakr@sita.aero>
2025-07-28 19:56:26 +0300

    Adding Migration seeding, product sorting and filtering

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -5,8 +5,10 @@ namespace Core.Interfaces;
 
 public interface IProductRepository
 {
-    Task<IReadOnlyList<Product>> GetProductsAsync();
+    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
     Task<Product?> GetProductByIdAsync(int id);
+    Task<IReadOnlyList<string>> GetTypesAsync();
+    Task<IReadOnlyList<string>> GetBrandsAsync();
     void AddProduct(Product product);
     void UpdateProduct(Product product);
     void DeleteProduct(Product product);
