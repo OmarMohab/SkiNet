8d64e10b6d32700e87dcdaaf0a30b2192d92e8f5
Omar Bakr <omarmohab221@gmail.com>
2025-08-11 11:13:30 +0300

    Adding Clinet Stripe Service

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51Rue3ECfaEXUCOhMFJNjqPpb6evWknrYJgVx4AyooYe6HZNO09JOEEvRyCVoiyenYKy1UmUpYya6btRdSacN5uhI00vabao8Rh'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51Rue3ECfaEXUCOhMFJNjqPpb6evWknrYJgVx4AyooYe6HZNO09JOEEvRyCVoiyenYKy1UmUpYya6btRdSacN5uhI00vabao8Rh'
+
 };
