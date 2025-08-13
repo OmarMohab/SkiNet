44d98fbd7ff6b09b5a05c511ab8384ce6a0b9a0e
Omar Bakr <omarmohab221@gmail.com>
2025-08-13 07:47:51 +0300

    Adding GetOrdersForUser and GetOrderById methods in OrdersController. and OrderSpecification

---
+++ Core/Specifications/OrderSpecification.cs
@@ -0,0 +1,17 @@
+using System.Security.Cryptography.X509Certificates;
+using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications;
+
+public class OrderSpecification : BaseSpecification<Order>
+{
+    public OrderSpecification(string email) : base(x => x.BuyerEmail == email)
+    {
+
+    }
+
+    public OrderSpecification(string email, int id) : base(x => x.BuyerEmail == email && x.Id == id)
+    {
+
+    }
+}
