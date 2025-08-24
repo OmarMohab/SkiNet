6b01c9b71ce015e135370e6efd34a01a44737ec8
Omar Bakr <omarmohab221@gmail.com>
2025-08-24 20:31:54 +0300

    replacing Guid with static IDs for roles to fix RolesAddedMibgration issue

--- Infrastructure/Config/RoleConfiguration.cs
+++ Infrastructure/Config/RoleConfiguration.cs
@@ -9,8 +9,8 @@ public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
     public void Configure(EntityTypeBuilder<IdentityRole> builder)
     {
         builder.HasData(
-            new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Admin", NormalizedName = "ADMIN" },
-            new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Customer", NormalizedName = "CUSTOMER" }
+            new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+            new IdentityRole { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" }
         );
     }
 }
---
+++ Infrastructure/Migrations/20250824171011_RolesAddedWithStaticIds.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesAddedWithStaticIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6048de89-7559-4f3a-971e-8c76a2f0a181");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c155ef7c-fd90-4491-a930-622fb1b4847c");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1", null, "Admin", "ADMIN" },
+                    { "2", null, "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6048de89-7559-4f3a-971e-8c76a2f0a181", null, "Admin", "ADMIN" },
+                    { "c155ef7c-fd90-4491-a930-622fb1b4847c", null, "Customer", "CUSTOMER" }
+                });
+        }
+    }
+}
